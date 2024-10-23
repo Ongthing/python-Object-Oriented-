35e4afb2bdc7beedcfd04775ee5834fbd609326f
Ongthing Marma <pujabarua01905096129@gmail.com>
2024-10-23 17:41:29 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+#python Object Oriented Programming
+
+from car import Car
+car_1 = Car("ongthing","OTL",2027,"yello")
+car_2 = Car("ford","Mustang",2011,"red")
+# print(car_1.make)
+# print(car_1.model)
+# print(car_1.year)
+# print(car_1.color)
+# print(car_2.make)
+# print(car_2.model)
+# print(car_2.year)
+# print(car_2.color)
+
+
+
+#car_2.drive()
+# car_2.stop()
+#car_2.drive()
+#car_1.stop()
